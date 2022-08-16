ea6b24fcef7766409f0ac897b1929bb0472b3d45
Rhoda Gachimu <gachimurhoda@gmail.com>
2022-08-16 14:10:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+
+document.body.removeChild(main);
+var newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "Rhoda is the champion";
